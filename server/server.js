bfd10f3c80a7c6a6e7ed0d37e7ab3fb6db0dd947
mihir224 <mihirsaini25@gmail.com>
2023-10-23 19:58:55 +0530

    change cors config

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const server=createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:['http://localhost:3000','https://chatzilla.netlify.app']
+        origin:['http://localhost:3000','https://chat-zilla.netlify.app']
     }
 });
 
