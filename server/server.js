4b83f346e6811330e4a84dc009e3fddd05cd655b
mihir224 <mihirsaini25@gmail.com>
2023-10-23 19:34:08 +0530

    minor changes for prod

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import dotenv from 'dotenv';
 
 const app=express();
+dotenv.config();
+
 const server=createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:'*'
+        origin:['http://localhost:3000','https://chatzilla.netlify.app']
     }
 });
 
+const PORT=process.env.PORT || 5000;
 io.on("connection",(socket)=>{
     console.log("user connected")
     socket.on("chat",(payload)=>{
@@ -18,8 +22,8 @@ io.on("connection",(socket)=>{
     })
 });
 
-server.listen(5000,()=>{
-    console.log("server started on port 5000");
+server.listen(PORT,()=>{
+    console.log("server started on port ", PORT);
 });
 
 
