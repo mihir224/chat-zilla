7be2be47123cb07dfac6d190dfc41910583a1f75
mihir224 <mihirsaini25@gmail.com>
2023-10-27 03:14:40 +0530

    added room functionality

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ import { createServer } from 'http';
 import dotenv from 'dotenv';
 
 const app=express();
+var currentUser={
+    userName:"",
+    room:""
+};
 dotenv.config();
 
 const server=createServer(app);
@@ -12,13 +16,23 @@ const io=new Server(server,{
         origin:['http://localhost:3000','https://chat-zilla.netlify.app']
     }
 });
-
+const setUserName=(user)=>{
+    currentUser={
+        userName:user.userName,
+        room:user.room
+    }
+}
 const PORT=process.env.PORT || 5000;
 io.on("connection",(socket)=>{
     console.log("user connected")
+    socket.on("room",({userName,room})=>{
+        setUserName({userName:userName,room:room});
+        socket.join(room);
+        socket.broadcast.to(room).emit("generated",`${userName} joined the chat`)
+    })
     socket.on("chat",(payload)=>{
         console.log("payload is: ",payload);
-        io.emit("chat",payload);
+        io.to(currentUser.room).emit("chat",payload);
     })
 });
 
