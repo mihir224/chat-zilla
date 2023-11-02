6438111b1c12b4f5f7c75d3feeeb5980b92a0c7e
mihir224 <mihirsaini25@gmail.com>
2023-11-03 03:56:56 +0530

    configured routes

--- server/server.js
+++ server/server.js
@@ -3,37 +3,55 @@ import { Server } from 'socket.io';
 import { createServer } from 'http';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoutes from './routes/auth.js';
+import chatRoutes from './routes/chat.js';
+import messageRoutes from './routes/message.js'
+import roomRoutes from './routes/room.js';
+import userRoutes from './routes/user.js';
+import cookieParser from 'cookie-parser';
 
 const app=express();
+
+dotenv.config();
 
 var currentUser={
     userName:"",
     room:""
 };
 
-dotenv.config();
-
 const connect=()=>{
-    mongoose.connect(`mongodb+srv://${process.env.MONGO}@cluster0.ly91ewg.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
+    mongoose.connect(`mongodb+srv://${process.env.MONGO}@cluster0.ly91ewg.mongodb.net/ChatZillaDB?retryWrites=true&w=majority`).then(()=>{
         console.log('DB Connected');
     }).catch(err=>{
         console.log(err);
     });
 }
 
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/auth',authRoutes);
+app.use('/api/chat',chatRoutes);
+app.use('/api/message',messageRoutes);
+app.use('/api/room',roomRoutes);
+app.use('/api/user',userRoutes);
+
 const server=createServer(app);
+
 const io=new Server(server,{
     cors:{
         origin:['http://localhost:3000','https://chat-zilla.netlify.app']
     }
 });
+
 const setUserName=(user)=>{
     currentUser={
         userName:user.userName,
         room:user.room
     }
 }
+
 const PORT=process.env.PORT || 5000;
+
 io.on("connection",(socket)=>{
     console.log("user connected")
     socket.on("room",({userName,room})=>{
@@ -48,7 +66,7 @@ io.on("connection",(socket)=>{
 });
 
 server.listen(PORT,()=>{
-    // connect();
+    connect();
     console.log("server started on port ", PORT);
 });
 
