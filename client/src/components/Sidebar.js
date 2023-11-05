4de1c51b1002e3680ba3c9968e3afa0f59707387
mihir224 <mihirsaini25@gmail.com>
2023-11-05 22:53:34 +0530

    users connected list

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import {useSelector} from 'react-redux';
 import '../styles/Sidebar.css';
+import axios from 'axios';
 
 function Sidebar(){
+    const currentRoom=useSelector(state=>state.room.currentRoom);
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+       if(currentRoom){
+           ( ()=>{
+            currentRoom.members.map(async(member_id)=>{
+                const member=await getUser(member_id);
+                setUsers((prev)=>[...prev,member]);
+            })
+           })();
+       }
+       
+    },[])    
+    const getUser=async(member_id)=>{
+        try{
+            const url=process.env.NODE_ENV==="production"?"https://chat-zilla-backend.onrender.com/api":"http://localhost:5000/api";
+            const res=await axios.get(`${url}/user/find/${member_id}`);
+            console.log(res)
+            return res.data;
+        }catch(err){
+            console.log(err);
+        }
+    }
     return (
         <div id='side-bar'>
         <div>
         <label id='list-label' htmlFor='user-list'>Users Connected</label>
         <ul className='sb-list' id='user-list'>
-            <li>FDSF</li>
-            <li>dadf</li>
+            {users.length>0&&users.map((user,index)=>{
+                return (<li key={index} style={{color:'white'}}>{user.name}</li>)
+            })}
         </ul>
         </div>
         <div>
