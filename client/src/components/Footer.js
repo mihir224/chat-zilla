d16b7a6f5abba2f0949bd0c0d42ca3ec5c9f1439
mihir224 <mihirsaini25@gmail.com>
2023-11-13 02:15:19 +0530

    added Footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer(){
+    return (
+        <div id='footer'>
+            <p>© Copyright Mihir Saini 2023</p>
+        </div>
+    )
+}
+
+export default Footer;
